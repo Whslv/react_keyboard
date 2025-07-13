3be45f9a0a02f4af0d8025be8b24fc538560e901
Whslv <vladon19977@gmail.com>
2025-07-13 19:52:55 -0400

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, {} from 'react';
+import React from 'react';
 
 type State = {
   key: string;
 };
 
-export class App extends React.Component<State> {
+export class App extends React.Component<{}, State> {
   state: State = {
     key: '',
   };
@@ -25,7 +25,7 @@ export class App extends React.Component<State> {
     const { key } = this.state;
 
     return (
-      <div className="App" onKeyDown={this.handleKeyPress}>
+      <div className="App">
         {key === '' ? (
           <p className="App__message">Nothing was pressed yet</p>
         ) : (
