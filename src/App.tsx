0e0efbb0b5542a06d1196be813259a1b636a3f08
Whslv <vladon19977@gmail.com>
2025-07-13 18:49:26 -0400

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import { event } from 'cypress/types/jquery';
+import React, { useEffect, useState } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export const App: React.FC = () => {
+  const [key, setKey] = useState('');
+
+  useEffect(() => {
+    const handlKeyPress = (event: React.KeyboardEvent) => {
+      setKey(event.key);
+    };
+    document.addEventListener('keyup', handlKeyPress);
+
+    return () => {
+      document.removeEventListener('keyup', handlKeyPress);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      {key === '' ? (
+        <p className="App__message">Nothing was pressed yet</p>
+      ) : (
+        <p className="App__message">The last pressed key is [{key}]</p>
+      )}
+    </div>
+  );
+};
